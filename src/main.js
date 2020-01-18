75d68d126b0511c7ea257a2bba6922c19d0fdb49
Andrew Euredjian <andrew_103@rocketmail.com>
2020-01-18 15:25:28 -0500

    fix small bug that killed everything

--- src/main.js
+++ src/main.js
@@ -21,6 +21,13 @@ module.exports.loop = function () {
     var energy_couriers = _.filter(Game.creeps, (creep) => creep.memory.role == 'energy_courier');
     // console.log('Harvesters: ' + harvesters.length);
 
+    if (energy_couriers.length < 3) {
+        var newName = 'Energy_Courier' + Game.time;
+        // console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([CARRY,CARRY,MOVE,MOVE], newName, 
+            {memory: {role: 'energy_courier'}});    
+    }
+    
     if(harvesters.length < 3) {
         var newName = 'Harvester' + Game.time;
         // console.log('Spawning new harvester: ' + newName);
@@ -28,13 +35,6 @@ module.exports.loop = function () {
             {memory: {role: 'harvester'}});
     }
 
-    else if (energy_couriers.length < 3) {
-        var newName = 'Energy_Courier' + Game.time;
-        // console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([CARRY,CARRY,MOVE,MOVE], newName, 
-            {memory: {role: 'energy_courier'}});    
-    }
-    
     else if(upgraders.length < 4) {
         var newName = 'Upgrader' + Game.time;
         // console.log('Spawning new harvester: ' + newName);
